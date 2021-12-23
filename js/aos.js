08b982842c834cc237b99cb9365426b18fa05da6
MuhammadRaihan27 <r41h4n.mrn@gmail.com>
2021-12-23 23:03:46 +0700

    last commit

--- js/aos.js
+++ js/aos.js
@@ -11,71 +11,73 @@ const imgFabrication = document.querySelectorAll("#fabrication img");
 const imgService = document.querySelectorAll("#services-img img");
 
 
-headingInfo.forEach((info, i) => {
-    info.dataset.aos = "fade-right";
-})
+if (window.innerWidth > 450) {
+    headingInfo.forEach((info, i) => {
+        info.dataset.aos = "fade-right";
+    })
 
-deks.forEach((info, i) => {
-    info.dataset.aos = "fade-up-right";
-    info.dataset.aosDuration = 1000;
-})
+    deks.forEach((info, i) => {
+        info.dataset.aos = "fade-up-right";
+        info.dataset.aosDuration = 1000;
+    })
 
-card.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    card.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-excessItem1.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    excessItem1.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-excessItem2.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    excessItem2.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-footer.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    footer.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-mision.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    mision.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-imgProduct.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    imgProduct.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-imgEngineeringDesain.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    imgEngineeringDesain.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-imgFabrication.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    imgFabrication.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
-imgService.forEach((info, i) => {
-    info.dataset.aos = "zoom-out-down";
-    info.dataset.aosDelay = i * 100;
-    info.dataset.aosDuration = 1000;
-})
+    imgService.forEach((info, i) => {
+        info.dataset.aos = "zoom-out-down";
+        info.dataset.aosDelay = i * 100;
+        info.dataset.aosDuration = 1000;
+    })
 
 
+    AOS.init({
+        once: true,
+    });
 
-AOS.init({
-    once: true,
-});
+}
